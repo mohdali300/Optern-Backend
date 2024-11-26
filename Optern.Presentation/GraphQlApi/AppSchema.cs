385cf34a1c6064683f50832cd2d15d04fdcc5281
glitch00 <abdelattybadwy166@gmail.com>
2024-11-26 13:51:44 +0200

    feat : setup postgresql and graphql

---
+++ Optern.Presentation/GraphQlApi/AppSchema.cs
@@ -0,0 +1,12 @@
+using GraphQL.Types;
+
+namespace Optern.Presentation.GraphQlApi
+{
+    public class AppSchema : Schema
+    {
+        public AppSchema()
+        {
+        	
+        }
+    }
+}
