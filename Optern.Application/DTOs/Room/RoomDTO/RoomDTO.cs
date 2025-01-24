6595ea36638638a39f71bfae086dc221bdfdde7c
Mohamed Fadel <fadelmohamed445@gmail.com>
2025-01-24 03:49:46 +0200

    3:49 on local branch

---
+++ Optern.Application/DTOs/Room/RoomDTO/RoomDTO.cs
@@ -0,0 +1,21 @@
+﻿using Optern.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optern.Application.DTOs.Room.RoomDTO
+{
+    public class RoomDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? Capacity { get; set; }
+        public RoomType RoomType { get; set; }
+        public string CoverPicture { get; set; }
+        public List<string> Tracks { get; set; } = new List<string>();
+        public List<string> SubTracks { get; set; } = new List<string>();
+        public List<string> Skills { get; set; } = new List<string>();
+    }
+}
