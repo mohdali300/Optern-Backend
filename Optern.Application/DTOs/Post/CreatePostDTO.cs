154851b1a32c312538aed2f14de89212a786cb6f
Menna Mohamed <mennamohammed178@gmail.com>
2025-01-28 04:52:12 +0200

    Feat(Post Service) : Create and Delete Post Endpoints

---
+++ Optern.Application/DTOs/Post/CreatePostDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optern.Application.DTOs.Post
+{
+    public class CreatePostDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public List<string>? Tags { get; set; } 
+    }
+}
