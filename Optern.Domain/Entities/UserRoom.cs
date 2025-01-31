2c2f48d100be53fc87d9e13dc6227747d06ff83a
mohdali300 <mohdali30060@gmail.com>
2025-01-31 18:53:45 +0200

    fix(Bookmarks):Edit BookMarkedTask relations and service

--- Optern.Domain/Entities/UserRoom.cs
+++ Optern.Domain/Entities/UserRoom.cs
@@ -13,12 +13,14 @@ namespace Optern.Domain.Entities
         public string RoomId { get; set; }
         public string UserId { get; set; }
        
-        public DateTime JoinedAt { get; set; }=DateTime.Now;
+        public DateTime JoinedAt { get; set; }=DateTime.UtcNow;
 
         public bool IsAdmin { get; set; }   
         // Navigation Property
 
         public virtual Room Room { get; set; }
         public virtual ApplicationUser User { get; set; }
+        public virtual ICollection<BookMarkedTask> BookMarkedTasks { get; set; }
+
     }
 }
