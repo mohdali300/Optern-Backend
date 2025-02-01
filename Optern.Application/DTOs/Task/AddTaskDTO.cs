8c7689be2e12f989240cb97a0ea20b239047ac3d
Menna Mohamed <mennamohammed178@gmail.com>
2025-02-01 05:54:32 +0200

    Feat(Task Service) : Edit - Delete Task

--- Optern.Application/DTOs/Task/AddTaskDTO.cs
+++ Optern.Application/DTOs/Task/AddTaskDTO.cs
@@ -10,32 +10,44 @@ namespace Optern.Application.DTOs.Task
 {
     public class AddTaskDTO
     {
+        public AddTaskDTO()
+        {
+            Title = string.Empty;  
+            Description = string.Empty; 
+            SprintId = 0; 
+            WorkSpaceId = 0;  
+            RoomId = string.Empty;  
+            Status = TaskState.ToDo;  
+            AssignedUserIds = new List<string>(); 
+            StartDate = string.Empty;  
+            DueDate = string.Empty;  
+        }
 
         [Required]
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         public string? Description { get; set; }
 
         [Required]
-        public int SprintId { get; set; }
+        public int? SprintId { get; set; }
 
         [Required]
-        public int WorkSpaceId { get; set; }
+        public int? WorkSpaceId { get; set; }
 
         [Required]
 
-        public string RoomId {  get; set; }
+        public string? RoomId {  get; set; }
 
         [Required]
-        public TaskState Status { get; set; }
+        public TaskState? Status { get; set; }
 
         [Required]
         public List<string> AssignedUserIds { get; set; } = new();
 
         [Required]
-        public string StartDate { get; set; }
+        public string? StartDate { get; set; }
 
         [Required]
-        public string DueDate { get; set; }
+        public string? DueDate { get; set; }
     }
 }
--- Optern.Application/Interfaces/ITaskService/ITaskService.cs
+++ Optern.Application/Interfaces/ITaskService/ITaskService.cs
@@ -11,6 +11,8 @@ namespace Optern.Application.Interfaces.ITaskService
    public interface ITaskService
     {
         public Task<Response<TaskResponseDTO>> AddTaskAsync(AddTaskDTO taskDto, string userId);
+        public Task<Response<TaskResponseDTO>> EditTaskAsync(EditTaskDTO editTaskDto, string userId);
+        public Task<Response<string>> DeleteTaskAsync(int taskId, string userId);
         public Task<Response<IEnumerable<RecentTaskDTO>>> GetRecentTasksAsync(string userId,string roomId,bool? isAdmin= false);
         public Task<Response<TasksSummaryDTO>> GetTasksSummaryAsync(string filterBy, string? roomId = null, int? sprintId = null);
     }
