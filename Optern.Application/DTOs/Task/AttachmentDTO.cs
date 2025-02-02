f4d3acfc4fbb9ad22937ef0e5bcd41754a221d67
Menna Mohamed <mennamohammed178@gmail.com>
2025-02-03 00:52:01 +0200

    Feat(Task Service) : Get Task Details Endpoint

---
+++ Optern.Application/DTOs/Task/AttachmentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optern.Application.DTOs.Task
+{
+    public class AttachmentDTO
+    {
+        public string Url { get; set; } = string.Empty;
+        public AssignedUserDTO Uploader { get; set; } = new();
+
+    }
+}
