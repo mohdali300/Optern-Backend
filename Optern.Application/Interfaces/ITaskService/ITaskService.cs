ebd099df061c7bcd8d702f243c68ad57095bfd64
Menna Mohamed <mennamohammed178@gmail.com>
2025-02-02 03:51:42 +0200

    Feat(Task Service) : Submit Task

--- Optern.Application/Interfaces/ITaskService/ITaskService.cs
+++ Optern.Application/Interfaces/ITaskService/ITaskService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Optern.Application.DTOs.Task;
+using Optern.Domain.Enums;
 using Optern.Infrastructure.Response;
 
 namespace Optern.Application.Interfaces.ITaskService
@@ -14,6 +15,8 @@ namespace Optern.Application.Interfaces.ITaskService
         public Task<Response<TaskResponseDTO>> EditTaskAsync(EditTaskDTO editTaskDto, string userId);
         public Task<Response<string>> DeleteTaskAsync(int taskId, string userId);
         public  Task<Response<TaskStatusGroupedDTO>> GetTasksByStatusAsync(GetTasksByStatusDTO request);
+
+        public Task<Response<string>> SubmitTaskAsync(int taskId, string userId, IFile? file, TaskState? newStatus);
 
         public Task<Response<IEnumerable<RecentTaskDTO>>> GetRecentTasksAsync(string userId,string roomId,bool? isAdmin= false);
         public Task<Response<TasksSummaryDTO>> GetTasksSummaryAsync(string filterBy, string? roomId = null, int? sprintId = null);
