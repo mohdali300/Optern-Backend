a22159b0b318d60b91b8c6c2bcb944f855f26eeb
Mohamed Fadel <fadelmohamed445@gmail.com>
2025-02-05 03:37:23 +0200

    feat(RepositoryFile): Add Search For Filles EndPoint

--- Optern.Application/Interfaces/IRepositoryFileService/IRepositoryFileService.cs
+++ Optern.Application/Interfaces/IRepositoryFileService/IRepositoryFileService.cs
@@ -14,5 +14,6 @@ namespace Optern.Application.Interfaces.IRepositoryFileService
         public Task<Response<RepositoryFileResponseDTO>> UploadFile(RepositoryFileDTO model, IFile file);
         public Task<Response<bool>> DeleteRepositoryFile(int repositoryFileId);
         public Task<Response<IEnumerable<RepositoryFileResponseDTO>>> GetUploadedFiles(int repositoryId, RepositoryFileSortType? sortType);
+        public Task<Response<IEnumerable<RepositoryFileResponseDTO>>> Search(int repositoryId, string pattern);
     }
 }
