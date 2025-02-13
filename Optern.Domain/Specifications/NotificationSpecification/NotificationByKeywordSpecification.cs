1f0f521083dd9db62f0d86c53cd90b7af0001ca5
Menna Mohamed <mennamohammed178@gmail.com>
2025-02-13 21:22:14 +0200

    Feat(UserNotifications Service - NotificationHub): Get-Search Notifications and Mark as Read

---
+++ Optern.Domain/Specifications/NotificationSpecification/NotificationByKeywordSpecification.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Optern.Domain.Entities;
+
+namespace Optern.Domain.Specifications.NotificationSpecification
+{
+
+    public class NotificationByKeywordSpecification : Specification<UserNotification>
+    {
+        private readonly string _keyword;
+
+        public NotificationByKeywordSpecification(string keyword)
+        {
+            _keyword = keyword?.Trim() ?? string.Empty; 
+        }
+
+        public override IQueryable<UserNotification> Apply(IQueryable<UserNotification> query)
+        {
+            return string.IsNullOrWhiteSpace(_keyword) ? query :
+             query.Where(n =>
+                 EF.Functions.Like(n.Notifications.Title, $"%{_keyword}%") ||
+                 EF.Functions.Like(n.Notifications .Message, $"%{_keyword}%"));
+        
+        }
+    }
+
+
+}
