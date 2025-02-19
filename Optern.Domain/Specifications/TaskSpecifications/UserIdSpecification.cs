c09f1684ff9c12f5a69076a4b9e820fe25e77d2a
glitch00 <abdelattybadwy166@gmail.com>
2025-02-19 21:15:30 +0200

    fix(taskWithFilter):fix userId problem and get tasks for specific user

---
+++ Optern.Domain/Specifications/TaskSpecifications/UserIdSpecification.cs
@@ -0,0 +1,18 @@
+﻿namespace Optern.Domain.Specifications.TaskSpecifications
+{
+	public class UserIdSpecification : Specification<Domain.Entities.Task>
+	{
+		private readonly string? _userId;
+
+		public UserIdSpecification(string? userId)
+		{
+			_userId = userId;
+		}
+
+		public override IQueryable<Entities.Task> Apply(IQueryable<Entities.Task> query)
+		{
+			 if (string.IsNullOrEmpty(_userId)) return query;
+			return query.Where(t => t.AssignedTasks.Any(u=>u.UserId == _userId));
+		}
+	}
+}
