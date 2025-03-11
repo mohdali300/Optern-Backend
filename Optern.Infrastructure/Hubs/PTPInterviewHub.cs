d4b131e6eb8dd62e360a81a485318ef042a897b0
Mohamed Fadel <fadelmohamed445@gmail.com>
2025-03-12 01:03:59 +0200

    feat(PTPInterviewHub): add Swap Role Between Users

--- Optern.Infrastructure/Hubs/PTPInterviewHub.cs
+++ Optern.Infrastructure/Hubs/PTPInterviewHub.cs
@@ -1,78 +1,84 @@
-﻿using System.Collections.Concurrent;
-using Task = System.Threading.Tasks.Task;
+﻿    using System.Collections.Concurrent;
+    using Task = System.Threading.Tasks.Task;
 
-namespace Optern.Infrastructure.Hubs
-{
-    public class PTPInterviewHub(IPTPInterviewService pTPInterviewService,IUserService userService):Hub
+    namespace Optern.Infrastructure.Hubs
     {
-        private readonly IPTPInterviewService _pTPInterviewService = pTPInterviewService;
-        private readonly IUserService _userService = userService;
-        private static readonly ConcurrentDictionary<string, HashSet<string>> _userConnectionMap = new(); // track user connections (from more than a device)
-
-        public override async Task OnConnectedAsync()
+        public class PTPInterviewHub(IPTPInterviewService pTPInterviewService,IUserService userService):Hub
         {
-            var user = await _userService.GetCurrentUserAsync();
-            if (user != null)
+            private readonly IPTPInterviewService _pTPInterviewService = pTPInterviewService;
+            private readonly IUserService _userService = userService;
+            private static readonly ConcurrentDictionary<string, HashSet<string>> _userConnectionMap = new(); // track user connections (from more than a device)
+
+            public override async Task OnConnectedAsync()
             {
-                var userId = user.Id;
-                _userConnectionMap.AddOrUpdate(
-                    userId,
-                    new HashSet<string> { Context.ConnectionId },
-                    (key, connections) =>
-                    {
-                        lock (connections)
+                var user = await _userService.GetCurrentUserAsync();
+                if (user != null)
+                {
+                    var userId = user.Id;
+                    _userConnectionMap.AddOrUpdate(
+                        userId,
+                        new HashSet<string> { Context.ConnectionId },
+                        (key, connections) =>
                         {
-                            connections.Add(Context.ConnectionId);
-                            return connections;
+                            lock (connections)
+                            {
+                                connections.Add(Context.ConnectionId);
+                                return connections;
+                            }
                         }
-                    }
-               );
+                   );
+                }
+                await base.OnConnectedAsync();
             }
-            await base.OnConnectedAsync();
-        }
 
-        [HubMethodName("JoinInterviewSession")]
-        public async Task JoinInterviewSession(int sessionId)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
-            await Clients.Caller.SendAsync("JoinToSession","Joined to interview session successfully.");
-        }
+            [HubMethodName("JoinInterviewSession")]
+            public async Task JoinInterviewSession(int sessionId)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
+                await Clients.Caller.SendAsync("JoinToSession","Joined to interview session successfully.");
+            }
 
-        [HubMethodName("EndInterviewSession")]
-        public async Task EndInterviewSession(int sessionId)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
-            await Clients.Caller.SendAsync("EndSession","Interview session ended.");
-        }
+            [HubMethodName("EndInterviewSession")]
+            public async Task EndInterviewSession(int sessionId)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
+                await Clients.Caller.SendAsync("EndSession","Interview session ended.");
+            }
 
-        [HubMethodName("UpdateCode")]
-        public async Task UpdateCode(int sessionId,string code)
-        {
-            await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("UpdatedCode", code);
-        }
+            [HubMethodName("UpdateCode")]
+            public async Task UpdateCode(int sessionId, string code)
+            {
+                await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("UpdatedCode", code);
+            }
+
+
 
         [HubMethodName("CodeOutput")]
-        public async Task CodeOutput(int sessionId, string output)
-        {
-            await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("CodeOutput", output);
-        }
-
-        public override async Task OnDisconnectedAsync(Exception? exception)
-        {
-            var user = await _userService.GetCurrentUserAsync();
-            if (user != null)
+            public async Task CodeOutput(int sessionId, string output)
             {
-                if (_userConnectionMap.TryGetValue(user.Id, out var userConnections))
+                await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("CodeOutput", output);
+            }
+
+            public async Task SwapRole(int sessionId, string userId)
+            {
+                await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("ReceiveOutput", userId);
+            }
+            public override async Task OnDisconnectedAsync(Exception? exception)
+            {
+                var user = await _userService.GetCurrentUserAsync();
+                if (user != null)
                 {
-                    lock (userConnections)
+                    if (_userConnectionMap.TryGetValue(user.Id, out var userConnections))
                     {
-                        userConnections.Remove(Context.ConnectionId);
-                        if (userConnections.Count == 0)
-                            _userConnectionMap.TryRemove(user.Id, out _);
+                        lock (userConnections)
+                        {
+                            userConnections.Remove(Context.ConnectionId);
+                            if (userConnections.Count == 0)
+                                _userConnectionMap.TryRemove(user.Id, out _);
+                        }
                     }
                 }
+                await base.OnDisconnectedAsync(exception);
             }
-            await base.OnDisconnectedAsync(exception);
         }
     }
-}
