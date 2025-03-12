cff59f70d53d8cba239bad8282078064f9111e6d
Mohamed Fadel <fadelmohamed445@gmail.com>
2025-03-12 18:57:23 +0200

    feat(PTPInterviewHub): add CountDown Timer in interview

--- Optern.Infrastructure/Hubs/PTPInterviewHub.cs
+++ Optern.Infrastructure/Hubs/PTPInterviewHub.cs
@@ -1,42 +1,56 @@
-﻿    using System.Collections.Concurrent;
-    using Task = System.Threading.Tasks.Task;
+﻿        using System.Collections.Concurrent;
+        using Task = System.Threading.Tasks.Task;
 
-    namespace Optern.Infrastructure.Hubs
-    {
-        public class PTPInterviewHub(IPTPInterviewService pTPInterviewService, IUserService userService) : Hub
+        namespace Optern.Infrastructure.Hubs
         {
-            private readonly IPTPInterviewService _pTPInterviewService = pTPInterviewService;
-            private readonly IUserService _userService = userService;
-            private static readonly ConcurrentDictionary<string, HashSet<string>> _userConnectionMap = new(); // track user connections (from more than a device)
-
-            public override async Task OnConnectedAsync()
+            public class PTPInterviewHub(IPTPInterviewService pTPInterviewService, IUserService userService, IHubContext<PTPInterviewHub> hubContext) : Hub
             {
-                var user = await _userService.GetCurrentUserAsync();
-                if (user != null)
+                private readonly IPTPInterviewService _pTPInterviewService = pTPInterviewService;
+                private readonly IUserService _userService = userService;
+                private static readonly ConcurrentDictionary<string, HashSet<string>> _userConnectionMap = new();
+                private readonly IHubContext<PTPInterviewHub> _hubContext= hubContext;
+                private static readonly ConcurrentDictionary<int, DateTime> _sessionEndTimeMap = new();
+                private static readonly ConcurrentDictionary<int, Timer> _sessionTimers = new();
+
+                public override async Task OnConnectedAsync()
                 {
-                    var userId = user.Id;
-                    _userConnectionMap.AddOrUpdate(
-                        userId,
-                        new HashSet<string> { Context.ConnectionId },
-                        (key, connections) =>
+                    try
+                    {
+                        var user = await _userService.GetCurrentUserAsync();
+                        if (user != null)
                         {
-                            lock (connections)
+                            var userId = user.Id;
+                            _userConnectionMap.AddOrUpdate(
+                                userId,
+                                new HashSet<string> { Context.ConnectionId },
+                                (key, connections) =>
+                                {
+                                    lock (connections)
+                                    {
+                                        connections.Add(Context.ConnectionId);
+                                        return connections;
+                                    }
+                                }
+                            );
+
+                            var currentInterviewResponse = await _pTPInterviewService.GetUserCurrentPTPInterviewSessionAsync(userId);
+                            if (currentInterviewResponse.IsSuccess && currentInterviewResponse.StatusCode == 200)
                             {
-                                connections.Add(Context.ConnectionId);
-                                return connections;
+                                int sessionId = currentInterviewResponse.Data.Id;
+                                await Groups.AddToGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
+                                await Clients.Caller.SendAsync("JoinToSession", "Joined to interview session successfully.");
+
+                                await BroadcastRemainingTime(sessionId);
                             }
                         }
-                    );
-                    // add user to running interview if he reconnect or connected from another device
-                    var currentInterviewResponse = await _pTPInterviewService.GetUserCurrentPTPInterviewSessionAsync(userId);
-                    if (currentInterviewResponse.IsSuccess && currentInterviewResponse.StatusCode == 200)
+                        await base.OnConnectedAsync();
+                    }
+                    catch (Exception e)
                     {
-                        await Groups.AddToGroupAsync(Context.ConnectionId, $"ptpInterview{currentInterviewResponse.Data.Id}");
-                        await Clients.Caller.SendAsync("JoinToSession", "Joined to interview session successfully.");
+                        Console.WriteLine(e);
+                    await Clients.Caller.SendAsync("JoinToSession", e.Message);
                     }
                 }
-                await base.OnConnectedAsync();
-            }
 
             [HubMethodName("JoinInterviewSession")]
             public async Task JoinInterviewSession(int sessionId, string userId)
@@ -48,81 +62,118 @@
                     {
                         await Groups.AddToGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
                         await Clients.Caller.SendAsync("JoinToSession", "Joined to interview session successfully.");
+
+                        if (!_sessionEndTimeMap.ContainsKey(sessionId))
+                        {
+                            var endTime = DateTime.UtcNow.AddHours(1);
+                            _sessionEndTimeMap[sessionId] = endTime;
+
+                            var timer = new Timer(async _ =>
+                            {
+                                await BroadcastRemainingTime(sessionId);
+                            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+                            _sessionTimers[sessionId] = timer;
+                        }
+
+                        await BroadcastRemainingTime(sessionId);
                     }
                     else
                     {
                         await Clients.Caller.SendAsync("FaliedJoinToSession", "Failed to start interview session.");
                     }
                 }
-                catch
+                catch(Exception ex)
                 {
-                    await Clients.Caller.SendAsync("JoinSessionError", "Unexpected error occurred while starting the session.");
+                    Console.WriteLine(ex);
+                    await Clients.Caller.SendAsync("JoinSessionError", "Server error try again");
+                }
+            }
+            private async Task BroadcastRemainingTime(int sessionId)
+            {
+                if (_sessionEndTimeMap.TryGetValue(sessionId, out var endTime))
+                {
+                    var remainingTime = endTime - DateTime.UtcNow;
+
+                    if (remainingTime.TotalSeconds <= 0)
+                    {
+                        if (_sessionTimers.TryRemove(sessionId, out var timer))
+                        {
+                            timer.Dispose();
+                        }
+                        _sessionEndTimeMap.TryRemove(sessionId, out _);
+                        await _hubContext.Clients.Group($"ptpInterview{sessionId}").SendAsync("TimerEnded", "The interview session time is over.");
+                    }
+                    else
+                    {
+                        await _hubContext.Clients.Group($"ptpInterview{sessionId}").SendAsync("UpdateTimer", remainingTime.ToString(@"hh\:mm\:ss"));
+                    }
                 }
             }
 
 
-
-
-    [HubMethodName("EndInterviewSession")]
-    public async Task EndInterviewSession(int sessionId)
-    {
-        try
+        [HubMethodName("EndInterviewSession")]
+        public async Task EndInterviewSession(int sessionId)
         {
-            var response = await _pTPInterviewService.EndPTPInterviewSessionAsync(sessionId);
-            if (response.IsSuccess)
+            try
             {
-                await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
-                await Clients.Caller.SendAsync("EndSession", "Interview session ended.");
-            }
-            else
-            {
-                await Clients.Caller.SendAsync("FailedEndSession", "Failed to end Interview session.");
-            }
-        }
-        catch
-        {
-            await Clients.Caller.SendAsync("EndSessionError", "Unexpected error occurred while ending session.");
-        }
-    }
-
-    [HubMethodName("UpdateCode")]
-    public async Task UpdateCode(int sessionId, string code)
-    {
-        await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("UpdatedCode", code);
-    }
-
-            [HubMethodName("CodeOutput")]
-    public async Task CodeOutput(int sessionId, string output)
-    {
-        await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("CodeOutput", output);
-    }
-
-    public async Task SwapRole(int sessionId, string userId)
-    {
-        await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("SwapRole", userId);
-    }
-
-    [HubMethodName("Language")]
-    public async Task Language(int sessionId, string language)
-    {
-        await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("ReceiveLanguage", language);
-    }
-            public override async Task OnDisconnectedAsync(Exception? exception)
-    {
-        var user = await _userService.GetCurrentUserAsync();
-        if (user != null)
-        {
-            if (_userConnectionMap.TryGetValue(user.Id, out var userConnections))
-            {
-                lock (userConnections)
+                var response = await _pTPInterviewService.EndPTPInterviewSessionAsync(sessionId);
+                if (response.IsSuccess)
                 {
-                    userConnections.Remove(Context.ConnectionId);
-                    if (userConnections.Count == 0)
-                        _userConnectionMap.TryRemove(user.Id, out _);
+                    await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"ptpInterview{sessionId}");
+                    await Clients.Caller.SendAsync("EndSession", "Interview session ended.");
+                }
+                else
+                {
+                    await Clients.Caller.SendAsync("FailedEndSession", "Failed to end Interview session.");
                 }
             }
-        }
-        await base.OnDisconnectedAsync(exception);
-    }
+            catch
+            {
+                await Clients.Caller.SendAsync("EndSessionError", "Unexpected error occurred while ending session.");
             }
         }
+
+        [HubMethodName("UpdateCode")]
+        public async Task UpdateCode(int sessionId, string code)
+        {
+             await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("UpdatedCode", code);
+        }
+
+         [HubMethodName("CodeOutput")]
+        public async Task CodeOutput(int sessionId, string output)
+        {
+            await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("CodeOutput", output);
+        }
+
+        [HubMethodName("SwapRole")]
+        public async Task SwapRole(int sessionId, string userId)
+        {
+            await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("SwapRole", userId);
+        }
+
+        [HubMethodName("Language")]
+        public async Task Language(int sessionId, string language)
+        {
+            await Clients.OthersInGroup($"ptpInterview{sessionId}").SendAsync("ReceiveLanguage", language);
+        }
+
+         public override async Task OnDisconnectedAsync(Exception? exception)
+         {
+            var user = await _userService.GetCurrentUserAsync();
+            if (user != null)
+            {
+                if (_userConnectionMap.TryGetValue(user.Id, out var userConnections))
+                {
+                    lock (userConnections)
+                    {
+                        userConnections.Remove(Context.ConnectionId);
+                        if (userConnections.Count == 0)
+                            _userConnectionMap.TryRemove(user.Id, out _);
+                    }
+                }
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+     }
+  }
