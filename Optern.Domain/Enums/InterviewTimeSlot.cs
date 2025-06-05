7df0d68ab9aa1a1101f192f108e243013722dace
Mohamed Fadel <fadelmohamed445@gmail.com>
2025-06-05 20:12:30 +0300

    pref(PTPInterview): allow user enter at any Time in day

--- Optern.Domain/Enums/InterviewTimeSlot.cs
+++ Optern.Domain/Enums/InterviewTimeSlot.cs
@@ -1,27 +1,79 @@
-﻿
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
 namespace Optern.Domain.Enums
 {
-
     public enum InterviewTimeSlot
     {
+        [Description("12:00 AM")]
+        TwelveAM,
+
+        [Description("01:00 AM")]
+        OneAM,
+
+        [Description("02:00 AM")]
+        TwoAM,
+
+        [Description("03:00 AM")]
+        ThreeAM,
+
+        [Description("04:00 AM")]
+        FourAM,
+
+        [Description("05:00 AM")]
+        FiveAM,
+
+        [Description("06:00 AM")]
+        SixAM,
+
+        [Description("07:00 AM")]
+        SevenAM,
+
         [Description("08:00 AM")]
         EightAM,
+
+        [Description("09:00 AM")]
+        NineAM,
 
         [Description("10:00 AM")]
         TenAM,
 
+        [Description("11:00 AM")]
+        ElevenAM,
+
         [Description("12:00 PM")]
         TwelvePM,
+
+        [Description("01:00 PM")]
+        OnePM,
 
         [Description("02:00 PM")]
         TwoPM,
 
+        [Description("03:00 PM")]
+        ThreePM,
+
+        [Description("04:00 PM")]
+        FourPM,
+
+        [Description("05:00 PM")]
+        FivePM,
+
         [Description("06:00 PM")]
         SixPM,
 
+        [Description("07:00 PM")]
+        SevenPM,
+
+        [Description("08:00 PM")]
+        EightPM,
+
+        [Description("09:00 PM")]
+        NinePM,
+
         [Description("10:00 PM")]
-        TenPM 
+        TenPM,
+
+        [Description("11:00 PM")]
+        ElevenPM
     }
 }
